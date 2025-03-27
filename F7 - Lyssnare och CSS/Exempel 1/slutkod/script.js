009952998bc6e08cad25643e6931283ed471ee5d
Pierre Sjöberg <pierre.sjoberg@kau.se>
2025-03-27 14:12:54 +0100

    F7 slutkod

--- F7 - Lyssnare och CSS/Exempel 1/slutkod/script.js
+++ F7 - Lyssnare och CSS/Exempel 1/slutkod/script.js
@@ -1,1 +1,60 @@
 'use strict';
+
+window.addEventListener('load', prepareColorPicker);
+
+
+function prepareColorPicker() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    let spans = document.querySelectorAll('span.value');
+
+    document.getElementById('red').value = r;
+    spans[0].textContent = r;
+
+    document.getElementById('green').value = g;
+    spans[1].textContent = g;
+
+    document.getElementById('blue').value = b;
+    spans[2].textContent = b;
+
+    document.querySelector('form').addEventListener('input', handleValueChange);
+
+    updateColor();
+
+}
+
+function handleValueChange(evt) {
+
+    //console.log(evt.target);
+    //console.log(evt.currentTarget);
+    let spans = document.querySelectorAll('span.value');
+
+    if(evt.target.getAttribute('id') === 'red') {
+        //console.log('Det är den röda!');
+        spans[0].textContent = evt.target.value;
+    }
+    else if(evt.target.getAttribute('id') === 'green') {
+       
+        spans[1].textContent = evt.target.value;
+    }
+    else {
+        spans[2].textContent = evt.target.value;
+
+    }
+
+    updateColor();
+
+}
+
+function updateColor() {
+    let r = document.querySelector('#red').value;
+    let g = document.querySelector('#green').value;
+    let b = document.querySelector('#blue').value;
+
+    let cssvalue = 'rgb(' + r + ',' + g + ',' + b + ')';
+
+    document.querySelector('body').style.backgroundColor = cssvalue;
+
+}
--- F7 - Lyssnare och CSS/Exempel 2/slutkod/script.js
+++ F7 - Lyssnare och CSS/Exempel 2/slutkod/script.js
@@ -1,1 +1,68 @@
 'use strict';
+let oGame = {};
+oGame.score = null;
+oGame.time = null;
+oGame.intervalId = null;
+
+
+window.addEventListener('load', prepareGame);
+
+
+function prepareGame() {
+    document.querySelector('#gameArea').classList.add('hidden');
+
+    let target = document.createElement('div');
+    target.classList.add('target');
+    document.querySelector('#gameArea').appendChild(target);
+
+    document.querySelector('.mybtn').addEventListener('click', startGame);
+}
+
+function startGame() {
+    document.querySelector('#gameArea').classList.remove('hidden');
+    oGame.score = 0;
+    oGame.time = 10;
+
+    //Skriv ut startvärden
+    document.querySelector('#score').textContent = 'Score: ' + oGame.score;
+    document.querySelector('#time').textContent = 'Time remaining: ' + oGame.time;
+
+    let target = document.querySelector('.target');
+    target.addEventListener('click', updateScore);
+
+    moveTarget();
+
+    oGame.intervalId = setInterval(moveTarget,1000);
+
+}
+
+
+function moveTarget() {
+
+    let target = document.querySelector('.target');
+    let gameArea = document.querySelector('#gameArea');
+
+    oGame.time--;
+    document.querySelector('#time').textContent = 'Time remaining: ' + oGame.time;
+
+    if(oGame.time<=0) {
+        //Spelet slut
+    }
+    else {
+        let left = Math.floor(Math.random() * (gameArea.offsetWidth-50));
+        let top = Math.floor(Math.random() * (gameArea.offsetHeight - 50));
+
+        target.style.left = left + 'px';
+        target.style.top = top + 'px';
+    }
+
+
+}
+
+function updateScore() {
+
+    oGame.score++;
+    document.querySelector('#score').textContent = 'Score: ' + oGame.score;
+
+
+}
