a936e31fb7011928540ce202205fb3ceb78bcd5a
Pierre Sjöberg <pierre.sjoberg@kau.se>
2025-03-28 09:30:32 +0100

    F8 startkod

---
+++ F8 - Skapa DOM-element/kodexempel_2/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let jsonobj ={
+	bilar: [
+		{
+			"id": 1,
+			"regnr": "ABC123",
+			"marke": "Volvo",
+			"modell": "V70 (III), 'FAS 2'",
+			"arsmodell": 2008,
+			"created_at": "2025-03-18T11:52:20.000000Z",
+			"updated_at": "2025-03-18T11:52:20.000000Z"
+		},
+		{
+			"id": 3,
+			"regnr": "QQQ111",
+			"marke": "Skoda",
+			"modell": "Oktavia",
+			"arsmodell": 2012,
+			"created_at": "2025-03-18T11:53:09.000000Z",
+			"updated_at": "2025-03-18T11:53:09.000000Z"
+		},
+		{
+			"id": 4,
+			"regnr": "WWW999",
+			"marke": "Trabant",
+			"modell": "Låda",
+			"arsmodell": 1967,
+			"created_at": "2025-03-18T11:53:44.000000Z",
+			"updated_at": "2025-03-18T11:53:44.000000Z"
+		},
+		{
+			"id": 5,
+			"regnr": "F1SK3N",
+			"marke": "KönigsÄgg",
+			"modell": "Skrothög",
+			"arsmodell": 1934,
+			"created_at": "2025-03-22T10:07:35.000000Z",
+			"updated_at": "2025-03-22T10:21:53.000000Z"
+		}
+	]
+};
